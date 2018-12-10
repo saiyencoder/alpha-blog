507e1e3c6e5fd216c0d755318d0e987b8ef93f35
Jesus Santizo <jsantizo@selectrehab.com>
2018-12-09 20:35:39 -0600

    Add secure passwords and attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_09_043934) do
+ActiveRecord::Schema.define(version: 2018_12_10_023206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_12_09_043934) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
