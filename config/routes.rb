863db9d8d1588295bc641c3e44b7791a04051e40
Jesus Santizo <jsantizo@selectrehab.com>
2018-12-10 20:17:27 -0600

    Add Categories Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
   
+  # Routes for Pages
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  # Routes for Articles
   resources :articles
 
+  # Routes for Users
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
 
+  # Routes for Sessions
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  # Routes for Categories
+  resources :categories, except: [:destroy]
+
+
 end
